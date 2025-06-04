9f4584b8aaf8edbcd43c2baead433d61258d5268
Mayura Jampasri <maimayura37620@gmail.com>
2025-06-04 09:21:08 +0700

    create authentication api and test to login

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,29 @@
 <template>
     <Navbar/>
     <p>This is a Dashboard page</p>
+    <p>Email login : {{ user?.email }}</p>
+
 </template>
 
 <script>
+import axios from 'axios';
 import Navbar from './Navbar.vue';
 
 //  สำหรับ import ค่าหรือ prop
 export default {
-    name: 'Dashboard'
+    name: 'Dashboard',
+    data(){
+        return{
+            user:null
+        }
+    },
+    async created(){
+        const response = await axios.get('user',{
+            withCredentials : true
+        });
+        console.log(response.data);
+        this.user = response.data
+    }
+
 }
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,53 +4,111 @@
         <div class="card-login">
             <h1>Login</h1>
             <p>Log in to your account</p>
-            <form @submit.prevent="onSubmit">
+
+            <form @submit.prevent="handleSubmit">
                 <!-- กดปุ่มแล้ว route ไปหน้า dashboard -->
                 <div class="form-group">
                     <label class="mb-2">Email</label>
-                    <input type="email" class="form-control" placeholder="Please enter your Email" id="email">
+                    <input type="email" class="form-control" placeholder="Please enter your Email" id="email"
+                        v-model="input.email" @blur="validate('email')" @keypress="validate('email')">
+                    <p class="errors" v-if="!!errors.email">{{ errors.email }}</p>
                 </div>
                 <div class="form-group">
                     <label class="mb-2">Password</label>
-                    <input type="password" class="form-control" placeholder="Please enter your password" id="password">
-                    <!-- <i class="bi bi-eye-slash" id="togglePassword"></i> -->
+                    <input type="password" class="form-control" placeholder="Please enter your password" id="password"
+                        v-model="input.password" @blur="validate('password')" @keypress="validate('password')">
+                    <p class="errors" v-if="!!errors.password">{{ errors.password }}</p>
                 </div>
                 <a href="#" style="color:#0057D8 ;">Forgot Password?</a>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
+
         </div>
         <div class="buttom-detail">
             <p>Copyright © 2025 ABC Company Limited. All rights reserved. </p>
             <a href="#">Privacy Policy I Terms & Conditions</a>
         </div>
-        <p>Welcome to basic fetch API => {{ firstName }} {{ lastName }}</p>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import * as Yup from 'yup';
+import Swal from 'sweetalert2'
 
 //  สำหรับ import ค่าหรือ prop
+
+const validateForm = Yup.object().shape({
+    email: Yup.string()
+        .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$/i, "Invalid email format")
+        .required("Please enter your Email"),
+    password: Yup.string()
+        .required("Please enter your Password")
+        .min(6, "password should be less thsn 6 characters"),
+})
+
 export default {
     name: 'Login',
     data() {
+        //   พื้นที่กำหนด data ที่ใช้งานใน component นี้
         return {
-            firstName: '',
-            lastName: ''
+            input: {
+                email: '',
+                password: ''
+            },
+            errors: {
+                email: '',
+                password: ''
+            }
         }
     },
-    mounted() {
-        this.getUser()
-    },
     methods: {
-        async getUser() {
-            await axios.get(`https://dummyjson.com/users`).then(res => {
-                const user = res.data.users[1];
-                this.firstName = user.firstName;
-                this.lastName = user.lastName;
-            }).catch(e => {
-                console.log(e);
+        // พื้นที่ใส่ function ที่เกี่ยวข้อง
+        handleSubmit() {
+            // ทำการ validate ข้อมูล
+            validateForm.validate(this.input, { abortEarly: false }).then(() => {
+                //เตรียมข้อมูลสำหรับส่งไป req
+                const data = {
+                    email: this.input.email,
+                    password: this.input.password
+                }
+                axios.post('user/login', data, {
+                    withCredentials: true
+                })
+                    .then(
+                        res => {
+                            console.log(res);
+                            Swal.fire({
+                                title: "Login success",
+                                icon: "success",
+                            });
+                            this.$router.push('/dashboard')
+                        }
+                    ).catch(err => {
+                        const errMessage = err.response.data.errMessage;
+                        console.log("Login error :", errMessage);
+                        Swal.fire({
+                            icon: "error",
+                            title: "Login fail",
+                            text: "ไม่สามารถเข้าสู่ระบบได้"
+                        });
+
+                    }
+                    )
             })
+                .catch((err) => {
+                    err.inner.array.forEach((error) => {
+                        this.errors = {
+                            ...this.errors, [error.path]: error.message
+                        };
+                    });
+                })
+        },
+        validate(field) {
+            validateForm.validateAt(field, this.input).then(() => (this.errors[field] = ""))
+                .catch((err) => {
+                    this.errors[err.path] = err.message;
+                })
         }
     }
 }
@@ -66,7 +124,7 @@ export default {
 
 .card-login {
     width: 485px;
-    height: 454px;
+    min-height: 454px;
     border: 1.5px solid #D7DDEC;
     border-radius: 10px;
     background-color: #F6F8FB;
@@ -119,5 +177,10 @@ form {
 .form-control :-ms-input-placeholder,
 .form-control::-ms-input-placeholder {
     color: #A6B0CF;
+}
+
+p.errors {
+    text-align: end;
+    color: red !important;
 }
 </style>
